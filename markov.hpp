2f02921e80e0ef448b433bf720fb425389917225
rgipsonhansen <rgipsonhansen@yahoo.com>
2020-12-06 19:53:35 -0700

    initial commit

---
+++ markov.hpp
@@ -0,0 +1,20 @@
+
+
+#infndef _MARKOV_H
+#define _MARKOV_H
+
+#include <string>
+#include <ctime>
+
+class markov {
+public:
+	markov();
+	void run();
+	void set_order();
+	void set_input_file();
+	void print();
+
+private:
+};
+
+#endif
