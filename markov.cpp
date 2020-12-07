cac53c6081a044eeeca3468ed0b616f9ec0cc063
rgipsonhansen <rgipsonhansen@yahoo.com>
2020-12-07 02:06:05 -0700

    generator wotks

--- markov.cpp
+++ markov.cpp
@@ -19,12 +19,13 @@ Markov::Markov(){
 
 void Markov::run(){
 	while (true) {
+		//print menu
 		cout<<"Predictive weather generation"<<endl;
 		cout<<"To set order, enter 1"<<endl;
 		cout<<"To enter a sequence, whos length matches the order, enter 2"<<endl;
-		cout<<"To get just the next day, enter 3"<<endl;
+		cout<<"To get stats for the next day, enter 3"<<endl;
 		cout<<"To generate a sequence of possible days, enter 4"<<endl;
-		cout<<"To exit, enter 5"<<endl;
+		cout<<"To exit, enter 9"<<endl;
 
 
 		string response = "";
@@ -34,6 +35,9 @@ void Markov::run(){
 
 		string order_string = "";
 		int order = 1;
+
+		string string_length = "";
+		int length = 0;
 		switch(option){
 			case 1:
 				cout<<"Enter an order: ";
@@ -45,16 +49,26 @@ void Markov::run(){
 				input_seed();
 				break;
 			case 3:
-				get_next_day();
+				get_stats();
 				break;
 			case 4:
-				generate_string();
+				cout<<"Enter a length of desired result: ";
+				getline(cin, string_length);
+				length = stoi(string_length);
+				generate_string(length);
 				break;
-			case 5:
+			case 9:
 				return;
 			default:
 				cout<<"Option not recognized"<<endl;
 		}
+		//flush the cin buffer
+		// string temp;
+		// cin.getline(temp, 64);
+		// if (cin.fail()) {
+		//     cin.clear();
+		//     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		// }
 	}
 }
 
@@ -84,7 +98,7 @@ void Markov::initialize(){
 		row++;
 	}
 
-
+	//find the column of data we are interested in
 	int index = 0;
 	for(auto name : rows[0]){
 		if(name == "SNWD"){
@@ -92,41 +106,79 @@ void Markov::initialize(){
 		}
 		index++;
 	}
+
+	//pull all usefull data from the desired column
 	string depth = "";
 	for(int i = 0; i < rows.size(); i++){
-		if(atoi(rows.at(i).at(snow_col).c_str()) > .5){
+		if(atoi(rows.at(i).at(snow_col).c_str()) > (double) 1 / NUM_OPTIONS){
 			depth = "1";
 		}else depth = "0";
 		depths.push_back(depth);
 	}
-	generate_map(order);
+	generate_map();
 }
 
 void Markov::set_order(int order){
 	this->order = order;
 	cout<<"Order has been set to "<<this->order<<endl;
 	cout<<endl;
+	generate_map();
+	return;
 }
 
 void Markov::set_input_file(){
 
 }
 
-void Markov::print(){
-
-}
-
 void Markov::input_seed(){
+	//initialize the markov seed
+	cout<<"Enter a string of 1s and 0s to start the string: ";
+	cin>>seed;
+	order = seed.length();
+	generate_map();
+}
+void Markov::get_stats(){
 
 }
-void Markov::get_next_day(){
 
+void Markov::generate_string(int length){
+
+	input_seed();
+
+	//generate the rand seed
+	srand(time(NULL));
+	int value = 0;
+	cout<<seed;
+	for(int i = 0; i < length - order; i++){
+		value = map_vector.at(order-1).at(seed).at(rand() % map_vector.at(order-1).at(seed).size());
+		cout<<value;
+		seed.erase(0,1);
+		seed.append(to_string(value));
+	}
+	cout<<endl;
+	cin.clear();
+	fflush(stdin);
+	return;
 }
 
-void Markov::generate_string(){
-
-}
-
-void Markov::generate_map(int order){
-
+void Markov::generate_map(){
+	//create keys, find the next value, and then increment the value at that value in the map
+	string key = "";
+	string_map.clear();
+	for(int i = 0; i < depths.size() - order - 1; i++ ){
+		key.clear();
+		for(int j = i; j < i + order; j++){
+			key +=depths.at(j);
+		}
+		string_map[key].push_back(stoi(depths.at(i + order)));
+	}
+	cout<<"Map with "<<string_map.size()<<" elements created (order "<< order<<")"<<endl;
+	if(map_vector.size() < order){
+		for(int i = map_vector.size(); i < order - 1; i++){
+			map_vector.push_back(map<string, vector<int>>());
+		}
+		map_vector.push_back(string_map);
+	}else(map_vector[order-1] = string_map);
+	cout<<endl<<endl;
+	return;
 }
--- markov.h
+++ markov.h
@@ -21,19 +21,20 @@ public:
 	void initialize();
 	void set_order(int order);
 	void set_input_file();
-	void print();
 	void input_seed();
-	void get_next_day();
-	void generate_string();
-	void generate_map(int order);
+	void get_stats();
+	void generate_string(int length);
+	void generate_map();
 
 private:
 	map<string, vector<int>> string_map;
 	vector<map<string, vector<int>>> map_vector;
-	int order = 1;
+	int order = 3;
+	string seed = "";
+	int NUM_OPTIONS = 2;
+
 
 	vector<string> depths;
-
 	string filepath = "Raw_Climate_Data.csv";
 	int snow_col = 5;
 
